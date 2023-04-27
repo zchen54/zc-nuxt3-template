4da43d15d9d5142c3cfbd62698453ee5bec0a210
zhuchen <171286434@qq.com>
2023-04-27 23:12:51 +0800

    update .gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,10 @@ export default defineNuxtConfig({
   //     script: [{ src: '@/assets/fonts/iconfont.js', type: 'text/javascript' }],
   //   },
   // },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
 })
